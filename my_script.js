a644a43b30aac6064030c175a437a14abf391c61
Suleman-Zakaria <72481089+Suleman-Zakaria@users.noreply.github.com>
2020-10-07 03:57:32 +0000

    Add files via upload

---
+++ my_script.js
@@ -0,0 +1,20 @@
+let elements = document.getElementsByName('cssProperty');
+
+let div = document.getElementById('modify');
+
+function set() {
+    "use strict";
+    //let elements = document.getElementsByName('cssProperty');
+    
+    for( let index = 0; index < elements.length; index++ ) 
+    {
+        let cssProperty = elements[ index ].getAttribute('id');
+        
+        let cssValue = elements[ index ].value;
+        console.log(cssProperty,cssValue);
+        
+        div.style[ cssProperty ] = cssValue;
+    }
+}
+
+document.getElementById('set').addEventListener('click',set);
